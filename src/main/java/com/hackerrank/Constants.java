8cb11e7aa7d80882c661bddab718065b400dd65a
akgolcukcan <akgolcuk.can@gmail.com>
2024-09-08 16:46:46 +0300

    first commit

---
+++ src/main/java/com/hackerrank/Constants.java
@@ -0,0 +1,7 @@
+package com.hackerrank;
+
+public class Constants {
+
+    public static final String OUTPUT_PATH = "/Users/canakgolcuk/CanCodes/HackerRank/stringSimilarity/src/main/resources/output/output.txt";
+
+}
--- src/main/java/com/hackerrank/Main.java
+++ src/main/java/com/hackerrank/Main.java
@@ -1,9 +1,51 @@
 package com.hackerrank;
 
+import java.io.*;
+import java.util.stream.IntStream;
+
+import static com.hackerrank.Constants.OUTPUT_PATH;
+
 public class Main {
-    public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
 
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(OUTPUT_PATH));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.stringSimilarity(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
+}
+
+class Result {
+
+    /*
+     * Complete the 'stringSimilarity' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static int stringSimilarity(String s) {
+        // Write your code here
+
+        return 0;
+    }
+
 }
